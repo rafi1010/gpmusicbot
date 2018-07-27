4bc66848ff03fd976088ff6c1519ae5ca48fa942
rafi1010 <41636364+rafi1010@users.noreply.github.com>
2018-07-27 04:46:14 +0300

    Update oyla.js

--- komutlar/oyla.js
+++ komutlar/oyla.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args, ops) => {
 
     let fetched = ops.active.get(message.guild.id);
 
-    if (!fetched) return message.channel.send('Bu Ses Kanalında Herhangi Bir Video Oynatılmıyor!');
+  if (!fetched) return message.channel.send('Bu Ses Kanalında Herhangi Bir Video Oynatılmıyor!');
 
     if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send('Afedersin, Botla Aynı Kanalda Değilsin.');
 
